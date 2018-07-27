74b1e4754d62f31cfd6bc20e9acc6e3565944719
BrightonG <Brighton.Gardiner@gmail.com>
2018-07-26 23:59:43 -0700

    Update MoveUVsTo01.py

--- MoveUVsTo01.py
+++ MoveUVsTo01.py
@@ -3,14 +3,18 @@ import math
 
 selection = pm.ls(sl=True)
 for item in selection:    
+    #Get Shape Node from selected Transform
     shape = pm.PyNode(item.node())
+    #Get the UV Length
     UVCount = len(shape.getUVs()[0])
     xRegion = 0
-    yRegion = 0    
+    yRegion = 0
+    #Calculate the UDIM Section by via Sum & Divide
     for i in xrange(0, UVCount):
         x, y = shape.getUV(i)
         xRegion = x + xRegion
         yRegion = y + yRegion
+    #Handle Negative UDIM sections
     if(yRegion < 0):
         yTemp = yRegion
     else:
@@ -25,6 +29,7 @@ for item in selection:
         yOffset = yOffset + -1
     if(xRegion < 0):
         xOffset = xOffset + -1
+    # Move each UV by the UDIM offset so that every UV is in 0-1 space.
     for i in xrange(0, UVCount):
         x, y = shape.getUV(i)
         newX = x - xOffset
