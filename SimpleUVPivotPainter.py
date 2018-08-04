d70791af5311c894f89bef75e25f8da8d9e3794e
BrightonG <Brighton.Gardiner@gmail.com>
2018-08-04 13:02:46 -0700

    Update SimpleUVPivotPainter.py

--- SimpleUVPivotPainter.py
+++ SimpleUVPivotPainter.py
@@ -1,32 +1,36 @@
-#WIP
 import pymel.core as pm
 
 #Setup UVSets for storing Pivots
 mesh = pm.ls(sl=True)[0].getShape()
 while len(pm.polyUVSet(mesh, auv = True, query = True )) < 4:
+    #Pivots are encoding into UV2 UV3
     pm.polyUVSet(mesh, copy = True)
     
 set = pm.polyUVSet(mesh, auv = True, query = True )
-print set
 setXY = set[2]
 setZ = set[3]
 
 pm.polyUVSet(mesh, rename = True, newUVSet='XY', uvSet= setXY)
 pm.polyUVSet(mesh, rename = True, newUVSet='Z', uvSet= setZ)
 
+selection = pm.ls(sl=True)
+
 pivot = pm.spaceLocator('PivotPoint')
 
-#pm.polyListComponentConversion(pm.ls(sl=True), tuv=True)
-#Select the UVS
-selection = pm.ls(sl=True)
-pm.polyUVSet(mesh, currentUVSet=True,  uvSet='XY')
-
-pm.polyUVSet(mesh, cuv = True, uvSet = "Z" )
+#Clear out existing XY.Z UVs
+pm.polyUVSet(mesh, cuv = True, uvSet = setZ )
 pm.select(pm.polyListComponentConversion(mesh, tuv=True))
-ZLen = pm.polyEvaluate(mesh, uvs = "Z", uvc = True)
+ZLen = pm.polyEvaluate(mesh, uvs = setZ, uvc = True)
 for i in xrange(ZLen):
-    u, v = mesh.getUV(i, uvSet = "Z")
-    mesh.setUV(i, u, 0, uvSet = "Z")
+    mesh.setUV(i, 0, 0, uvSet = setZ)
+
+pm.polyUVSet(mesh, cuv = True, uvSet = setXY )
+pm.select(pm.polyListComponentConversion(mesh, tuv=True))
+XYLen = pm.polyEvaluate(mesh, uvs = setXY, uvc = True)
+for i in xrange(XYLen):
+    mesh.setUV(i, 0, 0, uvSet = setXY)
+
+# Set UVs to world spaec coordinates of the locator
 x,y,z = pivot.translate.get()
 for vertex in selection:
     UVs = pm.polyListComponentConversion(vertex, tuv=True)
@@ -34,7 +38,7 @@ for vertex in selection:
         a = UV.split('[')[1][:-1]
         idList = list(a.split(':'))
         for id in idList:
-            mesh.setUV(id, x, y, uvSet = 'XY')
-            mesh.setUV(id, z, 1, uvSet = 'Z')
+            mesh.setUV(id, x, y, uvSet = setXY)
+            mesh.setUV(id, z, 1, uvSet = setZ)
     
     
