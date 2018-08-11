957cbccb7dbf77c1da8e54559a0bc6a09f4de1ee
BrightonG <Brighton.Gardiner@gmail.com>
2018-08-11 15:37:34 -0700

    refactoring
    Convert to a class-based approach for simpler handling

--- SimpleUVPivotPainter.py
+++ SimpleUVPivotPainter.py
@@ -1,5 +1,42 @@
 import pymel.core as pm
 
+selected = pm.ls(sl = True)
+#Refactoring into a class based approach
+
+class PivotPoint(object):
+    def __init__(self, original):
+        self.original = original
+        self.name = str(original)
+        self.shape = pm.PyNode(self.name.split(".")[0])
+        self.vPosition = original.getPosition()
+        
+        self.locator = pm.spaceLocator(name = self.name + '_loc')
+        self.locator.getTransform().setTranslation(self.vPosition, space = "world")
+    
+    def locatorPosition(self):
+        return self.locator.getTransform().getTranslation()
+    
+    def uvZ(self):
+        return (self.locatorPosition()[2], 0)
+    
+    def uvXU(self):
+        return (self.locatorPosition()[0], self.locatorPosition()[1])
+    
+    def UpdateUVs(self):
+        z = self.uvZ()
+        xy = self.uvXY()
+        print uv
+        self.original.setUV(z, uvSet = "Z")
+        self.original.setUV(xy, uvSet = "XY")
+        
+
+
+
+test = PivotPoint(selected[0])
+test.UpdateUVs()
+print test.locator.getTransform().getTranslation()
+print test.vPosition
+
 #Setup UVSets for storing Pivots
 mesh = pm.ls(sl=True)[0].getShape()
 while len(pm.polyUVSet(mesh, auv = True, query = True )) < 4:
@@ -13,7 +50,7 @@ setZ = set[3]
 pm.polyUVSet(mesh, rename = True, newUVSet='XY', uvSet= setXY)
 pm.polyUVSet(mesh, rename = True, newUVSet='Z', uvSet= setZ)
 
-selection = pm.ls(sl=True)
+selection = pm.polyListComponentConversion(pm.ls(sl=True), tv=True)
 
 pivot = pm.spaceLocator('PivotPoint')
 
@@ -43,10 +80,10 @@ pm.polyUVSet(mesh, cuv = True, uvSet = setXY )
 mesh.setUV(70, .5, .6, uvSet = setXY)
 for item in selection:
     print item
-#TODO: Selection is not iterating: 'pCubeShape1.vtx[12:27]'
+pm.select('pCube1.vtx[8:9]')
 for vertex in selection:
-    print vertex
     UVs = pm.polyListComponentConversion(vertex, tuv=True)
+    print type(UVs)
     for uv in UVs:
         a = uv.split('[')[1][:-1]
         if ":" in a:
